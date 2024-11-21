1fd6e13b6b19565ab524fff26aa251245d975fca
Sargius <Dr.Sargius@gmail.com>
2024-11-21 17:40:08 +0400

    - transfer changes to 70-10, part 4

--- core/interfaces/src/main/kotlin/app/aaps/core/interfaces/aps/Predictions.kt
+++ core/interfaces/src/main/kotlin/app/aaps/core/interfaces/aps/Predictions.kt
@@ -8,6 +8,6 @@ data class Predictions(
     var ZT: List<Int>? = null,
     var COB: List<Int>? = null,
     var aCOB: List<Int>? = null, // AMA only
-    var UAM: List<Int>? = null,
-    var TestAlex: List<Int>? = null
+    var UAM: List<Int>? = null
+    // var TestAlex: List<Int>? = null
 )
