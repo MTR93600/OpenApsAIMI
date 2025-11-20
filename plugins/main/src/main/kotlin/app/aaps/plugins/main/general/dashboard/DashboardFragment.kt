7d54927b08d6aa2181343dc37c8030da7e55ec06
mtr93600 <tellier@pm.me>
2025-11-20 22:47:13 +0100

    feat: Enable glucose graph viewport scrolling and scaling, and reposition update timestamp to the top-end.

--- plugins/main/src/main/kotlin/app/aaps/plugins/main/general/dashboard/DashboardFragment.kt
+++ plugins/main/src/main/kotlin/app/aaps/plugins/main/general/dashboard/DashboardFragment.kt
@@ -188,6 +188,10 @@ class DashboardFragment : DaggerFragment() {
         binding.statusCard.isFocusable = true
         binding.statusCard.setOnClickListener { openLoopDialog() }
         binding.glucoseGraph.graph.gridLabelRenderer?.gridColor = resourceHelper.gac(requireContext(), app.aaps.core.ui.R.attr.graphGrid)
+        binding.glucoseGraph.graph.viewport.isScrollable = true
+        binding.glucoseGraph.graph.viewport.isScalable = true
+        binding.glucoseGraph.graph.viewport.setScrollableY(false)
+        binding.glucoseGraph.graph.viewport.setScalableY(false)
         binding.glucoseGraph.graph.gridLabelRenderer?.reloadStyles()
     }
 
